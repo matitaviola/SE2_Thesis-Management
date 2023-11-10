5ef9d449efe41a4c4db25995eabc0d7026427ccb
matitaviola <megamattia2000@gmail.com>
2023-11-10 11:43:23 +0100

    Added unit tests for FE api

--- client/src/API.js
+++ client/src/API.js
@@ -13,7 +13,6 @@ const getApplications = async (user) =>{
     throw new Error("Error on getting the applications: Invalid role");
   }
   if(response.ok) {
-    console.log(applicationsJson);
     return applicationsJson.map(app => {
       return {"studentId": app.studentId, "proposal": app.proposal, "status":app.status
       //we'll need to add here the other fields of the application, when we'll know them
@@ -29,7 +28,7 @@ const getStudentData = async (proposalId, studentId) =>{
   const response = await fetch(SERVER_URL + `/api/application/${proposalId}/${studentId}`);
   const studentDataJson = await response.json();
   if(response.ok) {
-    console.log("nelle API"+JSON.stringify(studentDataJson)); //stringify needed, otherwise we'd have an [object Object]
+    //console.log("nelle API"+JSON.stringify(studentDataJson)); //stringify needed, otherwise we'd have an [object Object]
     return studentDataJson;
   }
   else{
