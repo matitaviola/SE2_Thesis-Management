1bb84d86dfd8b048f272dfe6813cae7dff1966ed
Fatemeh Maddahzadeh <fatemeh@MacBook-Pro-di-Antonio-2.local>
2023-11-13 05:29:44 +0100

    Insert proposal without test

--- server/index.js
+++ server/index.js
@@ -3,10 +3,13 @@ const loginDao = require('./DB/login-dao');
 const appDao = require('./DB/applications-dao');
 const propDao = require('./DB/proposals-dao');
 const studDao = require('./DB/students-dao');
+const supervisorDao = require('./DB/supervisors-dao');
+const degreeDao = require('./DB/degrees-dao');
 const express = require('express');
 const bodyParser = require ('body-parser')
 const cors = require('cors');
 const app = express();
+const moment = require('moment');
 const PORT = 3001;
 const authorizationMiddleware = require('./Middlewares/authorization-middleware');
 const dayjs = require('dayjs');
@@ -229,5 +232,37 @@ app.get('/api/proposals/students/:studentId',
     } catch (err){
       console.log(err);
       res.status(500).end();
+  }
+});
+app.post('/api/proposals', 
+  async (req, res) => {
+    try {
+      const { body } = req;
+      if (!(body.title && body.supervisor && body.co_supervisor && body.cds &&
+            body.keywords && body.type && body.groups && body.description && 
+            body.req_knowledge && body.notes && body.expiration && body.level))
+          throw new Error('Missing parameters');
+
+      if (moment(body.expiration).isBefore(moment()))
+        throw new Error('Invalid expiration date');
+      if (body.level !== 'BSc' && body.level !== 'MSc')
+        throw new Error('Invalid level');
+
+      const supervisor = await supervisorDao.getSupervisorById(body.supervisor);
+      if (!supervisor)
+        throw new Error('Invalid supervisor');
+
+      const degree = await degreeDao.getDegreeByCode(body.cds);
+      if (!degree)
+        throw new Error('Invalid degree');
+
+      const proposal = await propDao.addProposal(req.body);
+      res.json(proposal);
+  } catch (error) {
+      if (error === 'Duplicate title')
+        return res.status(400).json({ error: 'Duplicate title'});
+      if (error instanceof Error || error.code === 'SQLITE_ERROR')
+          return res.status(400).json({ error: error.message });
+      return res.status(500).json({ error: error.message || error })
   }
 });
