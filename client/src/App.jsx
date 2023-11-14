509c65af437c4a160aa42d6696c6a48a018f82fd
matitaviola <megamattia2000@gmail.com>
2023-11-14 14:55:02 +0100

    App, theman

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -25,13 +25,12 @@ useEffect(() => {
     const checkAuth = async () => {
       //const user = await API.getUserInfo(); // we have the user info here
       let user = {
-        /*
         role: 'TEACHER',
         id: 'd100003'
-        */
+        /*
         role: 'STUDENT',
         id: 's200000'
-        
+        */
       }
       setLoggedIn(user);
 
