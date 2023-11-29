ed1901a3c3bacfbd0807848cbc6ef2d19ea2c2c7
matitaviola <megamattia2000@gmail.com>
2023-11-29 11:46:50 +0100

    Added poweshell script for docker

---
+++ docker-build-n-push.ps1
@@ -0,0 +1,28 @@
+#Login
+Write-Host "To work, you need to have logged in to the repository"
+docker login
+
+# Run docker-compose build
+docker-compose build
+
+# Release number
+$release = "1"
+
+# Docker Hub repository
+$dockerhub_repo = "matitaviola/thesis_management_06"
+
+# Tags
+$server_tag = $dockerhub_repo+":server_release_"+$release
+$client_tag = $dockerhub_repo+":client_release_"+$release
+
+#Tag and push se2_thesis-management-backend
+docker tag "se2_thesis-management-backend:latest" "$server_tag"
+docker push "$server_tag"
+
+#Tag and push se2_thesis-management-frontend
+docker tag "se2_thesis-management-frontend:latest" "$client_tag"
+docker push "$client_tag"
+
+
+
+Write-Host "Build and push completed."
