ede05b26b4c5af1567c497549e3c810f9bb5ccc1
matitaviola <megamattia2000@gmail.com>
2023-11-29 16:38:26 +0100

    Added "back button"

--- client/src/components/CancelledProposalComponent.jsx
+++ client/src/components/CancelledProposalComponent.jsx
@@ -1,12 +1,30 @@
 import React from 'react';
-import '../App.css';
+import { Button, Container, Row, Col } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 const CancelledProposalMessage = () => {
   return (
-    <h2 className="centered-message">
-      This Proposal has been deleted by its supervisor. <br/>
-      Sorry for the inconvenience
-    </h2>
+    <Container>
+    <Row className="justify-content-center mt-4">
+      <Col>
+        <h1 className="text-center">This Proposal has been deleted by its supervisor.</h1>
+      </Col>
+    </Row>
+    <Row className="justify-content-center mt-4">
+      <Col>
+        <h2 className="lead text-center"> Sorry for the inconvenience</h2>
+      </Col>
+    </Row>
+    <Row className="justify-content-center mt-4">
+      <Col className="text-center">
+        <Button variant="primary">
+          <Link to="/proposals" style={{ textDecoration: 'none', color: 'inherit' }}>
+            PROPOSALS
+          </Link>
+        </Button>
+      </Col>
+    </Row>
+  </Container>
   );
 };
 
