1294b54c78305fc5b2fe2c6761c3aba1bb6e1ba7
matitaviola <megamattia2000@gmail.com>
2023-11-29 14:58:57 +0100

    Fixed Home

--- client/src/components/HomeComponent.jsx
+++ client/src/components/HomeComponent.jsx
@@ -1,9 +1,25 @@
-export default function HomeComponent() {
+import React from 'react';
+import { Container, Row, Col, Image } from 'react-bootstrap';
+import logo from '../assets/logo_orange.png';
 
-    return (
-        <>
-        <h1 className="lead">Home</h1>
-        <p className="lead">Nothing to see here</p>
-        </>
-    );
-  }
+export default function HomeComponent() {
+  return (
+    <Container>
+      <Row className="justify-content-center mt-4">
+        <Col>
+          <h1 className="text-center">Thesis Management - Team 6</h1>
+        </Col>
+      </Row>
+      <Row className="justify-content-center mt-4">
+        <Col>
+          <p className="lead text-center">Welcome to the Thesis Management System</p>
+        </Col>
+      </Row>
+      <Row className="justify-content-center mt-4">
+        <Col>
+          <Image src={logo} alt="Logo" fluid />
+        </Col>
+      </Row>
+    </Container>
+  );
+}
