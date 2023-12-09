1da12706afed3efe946b1b462811dfb18eb4fb2e
Forg-dev <s305930@studenti.polito.it>
2023-12-09 16:24:01 +0100

    Test Fix

--- server/Tests/notification.unit.test.js
+++ server/Tests/notification.unit.test.js
@@ -40,11 +40,15 @@ describe('Mail Server', () => {
     expect(nodemailer.createTransport).toHaveBeenCalledTimes(1);
 
     expect(nodemailer.createTransport).toHaveBeenCalledWith({
-      service: 'gmail',
       auth: {
-        user: 'groupsofteng6@gmail.com',
-        pass: 'afdmaktmymfaupmr',
+        pass: "afdmaktmymfaupmr",
+        user: "groupsofteng6@gmail.com",
       },
+      port: 465,
+      requireTLS: true,
+      secure: true,
+      secured: true,
+      service: "gmail",
     });
 
     expect(nodemailer.createTransport().sendMail).toHaveBeenCalledTimes(1);
@@ -81,11 +85,15 @@ describe('Mail Server', () => {
     expect(nodemailer.createTransport).toHaveBeenCalledTimes(1);
 
     expect(nodemailer.createTransport).toHaveBeenCalledWith({
-      service: 'gmail',
       auth: {
-        user: 'groupsofteng6@gmail.com',
-        pass: 'afdmaktmymfaupmr',
+        pass: "afdmaktmymfaupmr",
+        user: "groupsofteng6@gmail.com",
       },
+      port: 465,
+      requireTLS: true,
+      secure: true,
+      secured: true,
+      service: "gmail",
     });
 
     expect(nodemailer.createTransport().sendMail).toHaveBeenCalledTimes(1);
@@ -121,11 +129,15 @@ describe('Mail Server', () => {
     expect(nodemailer.createTransport).toHaveBeenCalledTimes(1);
 
     expect(nodemailer.createTransport).toHaveBeenCalledWith({
-      service: 'gmail',
       auth: {
-        user: 'groupsofteng6@gmail.com',
-        pass: 'afdmaktmymfaupmr',
+        pass: "afdmaktmymfaupmr",
+        user: "groupsofteng6@gmail.com",
       },
+      port: 465,
+      requireTLS: true,
+      secure: true,
+      secured: true,
+      service: "gmail",
     });
 
     expect(nodemailer.createTransport().sendMail).toHaveBeenCalledTimes(1);
